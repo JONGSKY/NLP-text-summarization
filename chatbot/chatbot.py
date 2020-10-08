34c1c54df251a3e7074e2094753e24d996541026
JONGSKY <ljhljh0125@gmail.com>
2020-10-08 19:30:23 +0900

    modify chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,7 +1,25 @@
 from flask import Flask, request, jsonify
 import sys
 app = Flask(__name__)
+import pandas as pd
 
+data = pd.read_csv('chatbot_data/20201008-19.csv')
+
+data_list = []
+for i in range(len(data[data['news_type'] == "today_main_news"])):
+    data_dict = {}
+
+    title = data[data['news_type'] == "today_main_news"].loc[i].title
+    description = data[data['news_type'] == "today_main_news"].loc[i].summarisation
+    imageUrl = data[data['news_type'] == "today_main_news"].loc[i].image_url
+    webLinkUrl = data[data['news_type'] == "today_main_news"].loc[i].news_url
+
+    data_dict['title'] = title
+    data_dict['description'] = description
+    data_dict['thumbnail'] = {"imageUrl":imageUrl}
+    data_dict['buttons'] = [{"action":  "webLink", "label": "뉴스기사 보러가기", "webLinkUrl": webLinkUrl}]
+    
+    data_list.append(data_dict)
 
 @app.route('/keyboard')
 def Keyboard():
@@ -25,50 +43,7 @@ def Message():
                     {
                         "carousel": {
                             "type": "basicCard",
-                            "items": [
-                                {
-                                    "title": "뉴스기사 제목",
-                                    "description": "뉴스기사 요약 3줄",
-                                    "thumbnail": {
-                                        "imageUrl": "https://imgnews.pstatic.net/image/081/2020/10/07/0003129476_001_20201007151624533.jpg?type=w647"
-                                    },
-                                    "buttons": [
-                                        {
-                                            "action":  "webLink",
-                                            "label": "뉴스기사 보러가기",
-                                            "webLinkUrl": "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=100&oid=081&aid=0003129476"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "title": "뉴스기사 제목",
-                                    "description": "뉴스기사 요약 3줄",
-                                    "thumbnail": {
-                                        "imageUrl": "https://imgnews.pstatic.net/image/081/2020/10/07/0003129476_001_20201007151624533.jpg?type=w647"
-                                    },
-                                    "buttons": [
-                                        {
-                                            "action":  "webLink",
-                                            "label": "뉴스기사 보러가기",
-                                            "webLinkUrl": "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=100&oid=081&aid=0003129476"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "title": "뉴스기사 제목",
-                                    "description": "뉴스기사 요약 3줄",
-                                    "thumbnail": {
-                                        "imageUrl": "https://imgnews.pstatic.net/image/081/2020/10/07/0003129476_001_20201007151624533.jpg?type=w647"
-                                    },
-                                    "buttons": [
-                                        {
-                                            "action":  "webLink",
-                                            "label": "뉴스기사 보러가기",
-                                            "webLinkUrl": "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=100&oid=081&aid=0003129476"
-                                        }
-                                    ]
-                                }
-                            ]
+                            "items": data_list
                         }
                     }
                 ]
